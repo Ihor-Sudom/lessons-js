c6fb46d0652bb2ef5a0bfed68f30bca626a4b031
Ihor <ihor.sudom@gmail.com>
2020-12-08 17:46:38 +0200

    option

---
+++ lesson-8-object/task-4-hw-2/index.js
@@ -0,0 +1,27 @@
+
+const user = {
+  name: 'Tom',
+  age: 17,
+};
+
+/* присвойте объекту свойство hobby со значением 'football' с помощью точки */
+user.hobby = 'footbal';
+
+const anotherKey = 'married';
+/* название свойства хранится в переменной anotherKey. Присвойте объекту такое свойство со значением false */
+user[`${anotherKey}`] = false;
+
+/* присвойте объекту свойство 'favorite music' со значением 'rock' */
+user['favorite music'] = 'rock';
+
+/* присвойте объекту свойство 'address', которое будет пустым объкетом */
+user['addres'] = {};
+
+/* присвойте объекту в 'address' свойство 'country', со значением 'Ukraine' */
+user.addres['country'] = 'Ukraine';
+
+/* присвойте объекту в 'address' свойство 'building', со значением 17 */
+user.addres['building'] = 17;
+
+/* выведите в консоль объект user */
+console.log(user);
