4de887596dba6fb3739e7045f349f4dce881379f
Ihor <ihor.sudom@gmail.com>
2020-12-08 21:31:59 +0200

    option

--- lesson-8-object/task-6-hw-4/index.js
+++ lesson-8-object/task-6-hw-4/index.js
@@ -13,14 +13,18 @@ function addPropertyV2(obj, key, value) {
 
 
 function addPropertyV3(obj, key, value) {
-  newObj = Object.assign({}, obj);
+  newObj = {};
   newObj[key] = value;
-  return newObj;
+  const sumObj = Object.assign(obj, newObj);
+  return sumObj;
 }
-
+console.log(addPropertyV3({A: 3, D: 5}, 'b', '50'));
 
 function addPropertyV4(obj, key, value) {
-  newObj = {...obj};
+  newObj = {};
   newObj[key] = value;
-  return newObj;
+  const sumObj = {...obj, ...newObj}
+  return sumObj;
 }
+
+
