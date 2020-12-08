1614bfe991d39e18a9e3c3d59dd35dc5ffae0e45
Ihor <ihor.sudom@gmail.com>
2020-12-08 17:29:37 +0200

    option

---
+++ lesson-8-object/task-2/index.js
@@ -0,0 +1,17 @@
+
+const obj = {
+  name: 'John Doe', 
+  age: 17, 
+  interest: 'football',
+};
+
+const concatProps = obj => {
+  let arr = [];
+    for (let key in obj) {
+      arr.push(obj[key]);
+    }
+  return arr;
+};
+
+console.log(concatProps(obj));
+
