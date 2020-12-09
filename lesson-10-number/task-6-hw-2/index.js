ef99d184c1cbdfb17699564715d78051c7f0ba66
Ihor <ihor.sudom@gmail.com>
2020-12-09 16:23:15 +0200

    option

--- lesson-10-number/task-6-hw-2/index.js
+++ lesson-10-number/task-6-hw-2/index.js
@@ -1,29 +1,15 @@
 
 function getRandomNumbers(len, start, end) {
-  if ((end - start) > 1) {
-    if (start > 0 && end > 0) {
-      return randomNumbersPlus(len, start, end);
-    }
-    return randomNumbersMinus(len, start, end);
-  } else {
-    return null;
+  if (Math.abs(end - start) > 1) {
+    return randomNumbersOptions(len, start, end);
   }
+ return null;
 }
 
-function randomNumbersPlus(len, start, end) {
-  let arr = [];
-  for (let i = 1; i <= len; i++) {
-    arr.push(Math.floor(Math.floor(start) + Math.random() * Math.floor(end - start)));
+function randomNumbersOptions(len, start, end) {
+  if (start > 0 && end > 0) {
+    return Array.from({ length: len }, (a, b) => Math.floor(Math.floor(start) + Math.random() * Math.floor(end - start)));
   }
-  return arr;
+    return Array.from({ length: len }, (a, b) => Math.floor(Math.ceil(start) + Math.random() * Math.ceil(end - start)));
 }
-
-function randomNumbersMinus(len, start, end) {
-  let arr = [];
-  for (let i = 1; i <= len; i++) {
-    arr.push(Math.floor(Math.ceil(start) + Math.random() * Math.ceil(end - start)));
-  }
-  return arr;
-}
-
 
