96a99eadf9dff092b20c4e73cdf6cb701152d87e
Ihor <ihor.sudom@gmail.com>
2020-12-11 17:25:50 +0200

    option

--- lesson-13-unit-test/task-3-hw-1/getMinSquaredNumber.js
+++ lesson-13-unit-test/task-3-hw-1/getMinSquaredNumber.js
@@ -2,3 +2,7 @@ export default (arr) {
   let result = arr.map(el => Math.abs(el));
   return Math.pow(Math.min.apply(null, result), 2);
 }
+
+
+
+
--- lesson-13-unit-test/task-3-hw-1/index.test.js
+++ lesson-13-unit-test/task-3-hw-1/index.test.js
@@ -1,6 +1,16 @@
 
 import getSquare from './getMinSquaredNumber';
 
+it('should get square of empty array', () => {
+  const result = getSquare([]);
+  expect(result).toEqual(Infinity);
+});
+
+it('should get square of string', () => {
+  const result = getSquare('hello');
+  expect(result).toEqual(NaN);
+});
+
 it('should get square of min number', () => {
   const result = getSquare([3, 4, 9, -8, -6]);
   expect(result).toEqual(9);
