f43545e1b50fa6b4542867faa7d870fce12e90b3
Ihor <ihor.sudom@gmail.com>
2020-12-19 15:48:49 +0200

    option

--- lesson-19-prototypes/task-1/index.js
+++ lesson-19-prototypes/task-1/index.js
@@ -5,25 +5,19 @@ export const vehicle = {
     console.log(`${this.name} is moving`);
   },
   stop() {
-    console.log(`${this.name} is stoped`);
+    console.log(`${this.name} stopped`);
   },
 }
 
 export const ship = {
-  liftAnchorUp() {
-    console.log(`${this.name} lifting anchor up`);
-  },
   startMachine() {
-    liftAnchorUp()
+    console.log(`${this.name} lifting anchor up`);
     this.move();
   },
   stopMachine() {
     this.stop();
-    this.liftAnchorDown();
-  },
-  liftAnchorDown() {
     console.log(`${this.name} lifting anchor down`);
-  }
+  },
 }
 
 Object.setPrototypeOf(ship, vehicle);
