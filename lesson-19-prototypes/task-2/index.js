0496f0dc2d92c00c5a4a2715e51483980615ca2e
Ihor <ihor.sudom@gmail.com>
2020-12-19 08:18:17 +0200

    option

--- lesson-19-prototypes/task-2/index.js
+++ lesson-19-prototypes/task-2/index.js
@@ -2,10 +2,9 @@
 export function getOwnProps(obj) {
   let arr = [];
   for (let prop in obj) {
-    if (hasOwnProperty(prop) && typeof obj[prop] !== 'function') {
+    if (obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function') {
       arr.push(prop);
     };
   };
   return arr;
 };
-
