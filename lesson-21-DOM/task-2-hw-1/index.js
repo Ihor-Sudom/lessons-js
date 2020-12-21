5a4aa096d6e9cb02886964f181555e5e3f84cd2e
Ihor <ihor.sudom@gmail.com>
2020-12-21 18:08:02 +0200

    option

---
+++ lesson-21-DOM/task-2-hw-1/index.js
@@ -0,0 +1,12 @@
+
+export const getTitleElement = () => {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
+}
+
+export const getInputElement = () => {
+  const inputElem = document.querySelector('type');
+  console.dir(inputElem);
+  return inputElem;
+}
