5c20eddff7c4f5cc3a8fb0729906503f42b865d1
Ihor <ihor.sudom@gmail.com>
2020-12-22 07:41:34 +0200

    option

--- lesson-21-DOM/task-1/index.js
+++ lesson-21-DOM/task-1/index.js
@@ -1,32 +1,53 @@
-const tasks = [
-  { text: 'Buy milk', done: false },
-  { text: 'Pick up Tom from airport', done: false },
-  { text: 'Visit party', done: false },
-  { text: 'Visit doctor', done: true },
-  { text: 'Buy meat', done: true },
+const tasks = [{
+    text: 'Buy milk',
+    done: false
+  },
+  {
+    text: 'Pick up Tom from airport',
+    done: false
+  },
+  {
+    text: 'Visit party',
+    done: false
+  },
+  {
+    text: 'Visit doctor',
+    done: true
+  },
+  {
+    text: 'Buy meat',
+    done: true
+  },
 ];
 
 const renderListItems = listItems => {
   const listElem = document.querySelector('.list');
+  const listLi = document.querySelectorAll('LI');
+  listElem.classList.remove('LI');
+  console.log(listLi);
+
 
   const listItemsElems = listItems
-        .sort((a, b) => a.done - b.done)
-        .map(({text, done}) => {
-          const listItemElem = document.createElement('li');
-          listItemElem.classList.add('list__item');
-          if (done) {
-            listItemElem.classList.add('list__item_done');
-          }
-          const checkboxElem = document.createEvent('input');
-          checkboxElem.setAttribute('type', 'checkbox');
-          checkboxElem.checked = done;
-          checkboxElem.classList.add('list__item-checkbox');
-          listItemElem.append(checkboxElem, text);
+    .sort((a, b) => a.done - b.done)
+    .map(({
+      text,
+      done
+    }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      const checkboxElem = document.createElement('input');
+      checkboxElem.setAttribute('type', 'checkbox');
+      checkboxElem.checked = done;
+      checkboxElem.classList.add('list__item-checkbox');
+      listItemElem.append(checkboxElem, text);
 
-          return listItemElem;
-        });
+      return listItemElem;
+    });
 
-        listElem.append(...listItemsElems);
+  listElem.append(...listItemsElems);
 }
 
 renderListItems(tasks);
