c615045564b239337282efad2b64009738ca8a61
Ihor <ihor.sudom@gmail.com>
2020-12-22 14:43:04 +0200

    option

--- lesson-21-DOM/task-1/index.js
+++ lesson-21-DOM/task-1/index.js
@@ -1,31 +1,6 @@
-const tasks = [{
-    text: 'Buy milk',
-    done: false
-  },
-  {
-    text: 'Pick up Tom from airport',
-    done: false
-  },
-  {
-    text: 'Visit party',
-    done: false
-  },
-  {
-    text: 'Visit doctor',
-    done: true
-  },
-  {
-    text: 'Buy meat',
-    done: true
-  },
-];
 
 const renderListItems = listItems => {
   const listElem = document.querySelector('.list');
-  const listLi = document.querySelectorAll('LI');
-  listElem.classList.remove('LI');
-  console.log(listLi);
-
 
   const listItemsElems = listItems
     .sort((a, b) => a.done - b.done)
@@ -43,11 +18,8 @@ const renderListItems = listItems => {
       checkboxElem.checked = done;
       checkboxElem.classList.add('list__item-checkbox');
       listItemElem.append(checkboxElem, text);
-
       return listItemElem;
     });
 
   listElem.append(...listItemsElems);
 }
-
-renderListItems(tasks);
