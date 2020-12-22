d560a33638e4df75f797c5bfc0355b2b6e0a9370
Ihor <ihor.sudom@gmail.com>
2020-12-22 14:44:27 +0200

    option

--- lesson-21-DOM/task-1/index.js
+++ lesson-21-DOM/task-1/index.js
@@ -1,4 +1,12 @@
 
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
 const renderListItems = listItems => {
   const listElem = document.querySelector('.list');
 
