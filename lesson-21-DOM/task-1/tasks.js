edb3099723925b5f4f6fa0a43c1fe13e56bffab9
Ihor <ihor.sudom@gmail.com>
2020-12-22 15:13:07 +0200

    option

--- lesson-21-DOM/task-1/tasks.js
+++ lesson-21-DOM/task-1/tasks.js
@@ -6,29 +6,21 @@ const tasks = [
   { text: 'Visit doctor', done: true },
   { text: 'Buy meat', done: true },
 ];
-
 const renderListItems = listItems => {
   const listElem = document.querySelector('.list');
-
-  const listItemsElems = listItems
-    .sort((a, b) => a.done - b.done)
-    .map(({
-      text,
-      done
-    }) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      const checkboxElem = document.createElement('input');
-      checkboxElem.setAttribute('type', 'checkbox');
-      checkboxElem.checked = done;
-      checkboxElem.classList.add('list__item-checkbox');
-      listItemElem.append(checkboxElem, text);
-      return listItemElem;
-    });
-
+  const listItemsElems = listItems.map(itemText => {
+    const listItemElem = document.createElement('li');
+    listItemElem.classList.add('list__item');
+    if (itemText.done === true) {
+      listItemElem.classList.add('list__item_done');
+    }
+    const checkboxElem = document.createElement('input');
+    checkboxElem.setAttribute('type', 'checkbox');
+    checkboxElem.classList.add('list__item-checkbox');
+    listItemElem.append(checkboxElem, itemText.text);
+    return listItemElem;
+  });
   listElem.append(...listItemsElems);
-}
+};
+renderListItems(tasks);
 
