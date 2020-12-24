cc141d7079a8b2d1212fb9d94a86bdc609683e57
Ihor <ihor.sudom@gmail.com>
2020-12-24 17:46:09 +0200

    option

--- lesson-24-work-date/task-3/index.js
+++ lesson-24-work-date/task-3/index.js
@@ -1,7 +1,8 @@
 
 
 export const getDiff = (startDate, endDate) => {
-  const differenceDay = new Date(startDate).getDate() - new Date(endDate).getDate();
+
+  const differenceDay = Math.round((new Date(startDate).getTime() - new Date(endDate).getTime()) / (60 * 60 * 24 * 1000));
   const differenceHours = new Date(startDate).getHours() - new Date(endDate).getHours();
   const differenceMinutes = new Date(startDate).getMinutes() - new Date(endDate).getMinutes();
   const differenceSeconds = new Date(startDate).getSeconds() - new Date(endDate).getSeconds();
@@ -10,5 +11,7 @@ export const getDiff = (startDate, endDate) => {
 };
 
 
-/* const result = getDiff(new Date(2019, 1, 1, 14, 15, 12), new Date(2019, 1, 8, 18, 56, 42));
-console.log(result); */
+/* const result = getDiff(new Date(), new Date(2020, 11, 15, 16, 10, 0));
+console.log(result);
+console.log(new Date());
+console.log(new Date(2020, 11, 15, 16, 10, 0)); */
