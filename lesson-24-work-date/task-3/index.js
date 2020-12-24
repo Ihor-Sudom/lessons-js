30e1cb06c37d0e028ba82ecdb9f626aee05b39f5
Ihor <ihor.sudom@gmail.com>
2020-12-24 19:59:28 +0200

    option

--- lesson-24-work-date/task-3/index.js
+++ lesson-24-work-date/task-3/index.js
@@ -3,8 +3,8 @@
 export const getDiff = (startDate, endDate) => {
   const _MS_PER_DAY = 1000 * 60 * 60 * 24;
 
-  const newStartDate = Date.UTC(new Date(startDate).getFullYear(), new Date(startDate).getMonth(), new Date(startDate).getDate());
-  const newEndDate = Date.UTC(new Date(endDate).getFullYear(), new Date(endDate).getMonth(), new Date(endDate).getDate());
+  const newStartDate = new Date(new Date(startDate).getFullYear(), new Date(startDate).getMonth(), new Date(startDate).getDate());
+  const newEndDate = new Date(new Date(endDate).getFullYear(), new Date(endDate).getMonth(), new Date(endDate).getDate());
 
   const differenceDay = (newStartDate - newEndDate) / _MS_PER_DAY;
   const differenceHours = new Date(startDate).getHours() - new Date(endDate).getHours();
@@ -13,6 +13,5 @@ export const getDiff = (startDate, endDate) => {
 
   return `${differenceDay}d ${differenceHours}h ${differenceMinutes}m ${differenceSeconds}s`;
 };
-
 
 
