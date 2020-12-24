44d893d07287442b969d9b74d312f93804c40cf3
Ihor <ihor.sudom@gmail.com>
2020-12-24 15:39:58 +0200

    option

---
+++ lesson-24-work-date/task-1/index.js
@@ -0,0 +1,13 @@
+const dayWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
+const dayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+
+  return dayWeek[new Date(dateInFuture).getDay()];
+}
+
+
+/* const result = dayOfWeek(new Date(2019, 1, 1), 20);
+
+console.log(result); */
