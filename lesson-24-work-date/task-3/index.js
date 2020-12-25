2a7b2268b300d2a092c350145b0bde02aa457d1b
Ihor <ihor.sudom@gmail.com>
2020-12-25 11:24:51 +0200

    option

--- lesson-24-work-date/task-3/index.js
+++ lesson-24-work-date/task-3/index.js
@@ -1,17 +1,14 @@
 
-
 export const getDiff = (startDate, endDate) => {
   const _MS_PER_DAY = 1000 * 60 * 60 * 24;
 
-  const newStartDate = new Date(new Date(startDate).getFullYear(), new Date(startDate).getMonth(), new Date(startDate).getDate());
-  const newEndDate = new Date(new Date(endDate).getFullYear(), new Date(endDate).getMonth(), new Date(endDate).getDate());
-
-  const differenceDay = (newEndDate - newStartDate) / _MS_PER_DAY;
-  const differenceHours = new Date(endDate).getHours() - new Date(startDate).getHours();
-  const differenceMinutes = new Date(endDate).getMinutes() - new Date(startDate).getMinutes();
-  const differenceSeconds = new Date(endDate).getSeconds() - new Date(startDate).getSeconds();
+  const differenceDay = Math.trunc(Math.abs((endDate - startDate) / _MS_PER_DAY));
+  const differenceHours = Math.abs(new Date(endDate).getHours() - new Date(startDate).getHours());
+  const differenceMinutes = Math.abs(new Date(endDate).getMinutes() - new Date(startDate).getMinutes());
+  const differenceSeconds = Math.abs(new Date(endDate).getSeconds() - new Date(startDate).getSeconds());
 
   return `${differenceDay}d ${differenceHours}h ${differenceMinutes}m ${differenceSeconds}s`;
 };
 
-
+const result = getDiff(new Date(2020, 2, 10, 0, 15, 59), new Date(2018, 2, 10, 9, 45, 45));
+console.log(result);
