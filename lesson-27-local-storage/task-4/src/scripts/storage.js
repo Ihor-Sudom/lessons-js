dfee107c5b01404041d40f23e501cb3d4a9a9a61
Ihor <ihor.sudom@gmail.com>
2020-12-28 17:29:12 +0200

    option

--- lesson-27-local-storage/task-4/src/scripts/storage.js
+++ lesson-27-local-storage/task-4/src/scripts/storage.js
@@ -1,8 +1,9 @@
 
 
 export const setItem = (key, value) => {
+  localStorage.clear();
   localStorage.setItem(key, JSON.stringify(value));
-  console.log(localStorage);
+  console.log(localStorage.tasksList);
 };
 
 export const getItem = key => JSON.parse(localStorage.getItem(key));
