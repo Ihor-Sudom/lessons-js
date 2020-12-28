ab3d2c0324d4b054ea16a14bfadb7b20ff88fce1
Ihor <ihor.sudom@gmail.com>
2020-12-28 09:00:43 +0200

    option

--- lesson-27-local-storage/task-1/index.js
+++ lesson-27-local-storage/task-1/index.js
@@ -1,10 +1,13 @@
 
+
+
 export const getLocalStorageData = () => {
   return Object.entries(localStorage)
     .reduce((acc, [key, value]) => {
       return {
         ...acc,
-        [key]: JSON.parse(value),
+        [key]: value,
       }
     }, {});
 }
+
