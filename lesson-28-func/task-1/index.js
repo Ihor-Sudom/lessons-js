7bc7a115cf13d1ec12b4d27cd732849f96caffd3
Ihor <ihor.sudom@gmail.com>
2020-12-29 12:34:24 +0200

    option

---
+++ lesson-28-func/task-1/index.js
@@ -0,0 +1,28 @@
+
+const calc = number => {
+  let result = number;
+
+  const calculator = {
+    add(value) {
+      result += velue;
+      return this;
+    },
+
+    mult(value) {
+      result *= value;
+      return this;
+    },
+
+    subtract(value) {
+      result -= value;
+      return this;
+    },
+
+    div(value) {
+      result /= value;
+      return this;
+    },
+  };
+
+  return result;
+};
