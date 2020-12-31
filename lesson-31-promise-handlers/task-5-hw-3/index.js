9d0942b35060f14b84ed818d38f5329ab709a9f5
Ihor <ihor.sudom@gmail.com>
2020-12-31 07:38:59 +0200

    option

--- lesson-31-promise-handlers/task-5-hw-3/index.js
+++ lesson-31-promise-handlers/task-5-hw-3/index.js
@@ -1,6 +1,7 @@
 
-export const delay = (time) => new Promise ((resolve) => {
+export const delay = time => new Promise ((resolve) => {
   setTimeout =(() => {
     console.log('Done');
+    resolve(time);
   }, time);
 });
