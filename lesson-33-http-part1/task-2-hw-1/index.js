3c78c1215fc002fcbbb7f0b4d03c2d1ccf373b3b
Ihor <ihor.sudom@gmail.com>
2021-01-02 13:59:10 +0200

    option

--- lesson-33-http-part1/task-2-hw-1/index.js
+++ lesson-33-http-part1/task-2-hw-1/index.js
@@ -1,17 +1,14 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 /* getTasksList code here */
-const getTasksList = () => {
-  return fetch('https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks')
+export const getTasksList = () => fetch(`${baseUrl}`)
     .then(response => response.json());
-};
 
 
 /* getTaskById code here */
-const getTaskById = (taskId) => {
-  return fetch('https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks')
+export const getTaskById = (taskId) => fetch(`${baseUrl}`)
   .then(response => response.json())
   .then(response => response[taskId - 1]);
-};
+
 
 
