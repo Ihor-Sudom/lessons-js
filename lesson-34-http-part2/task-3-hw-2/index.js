c78b1b1f98bd1a127b3d807a29fba719fd6f8b80
Ihor <ihor.sudom@gmail.com>
2021-01-05 18:14:38 +0200

    option

--- lesson-34-http-part2/task-3-hw-2/index.js
+++ lesson-34-http-part2/task-3-hw-2/index.js
@@ -1,27 +1,45 @@
 const baseUrl = 'https://5ff2e7d128c3980017b18ca3.mockapi.io/api/v1/form';
 
-const submittingFormData = () => {
-  const inputElem = document.querySelectorAll('.form-input');
-  const [email, userName, password] = [...inputElem].map(el => el.value);
+const emailElem = document.querySelector('#email');
+const textElem = document.querySelector('#name');
+const passwordElem = document.querySelector('#password');
+
+const errorElem = document.querySelector('.error-text');
+
+
+const submittingFormData = (event) => {
+  event.preventDefault();
 
   const user = {
-    email,
-    userName,
-    password,
+    email: emailElem.value,
+    userName: textElem.value,
+    password: passwordElem.value,
   };
-
+ 
   createUserForm(user)
+    .then(() => changeForm())
+    .then(() => getUserForm());
 }
 
-const submitButtonElem = document.querySelector('.submit-button');
-submitButtonElem.addEventListener('click', submittingFormData);
+  const submitButtonElem = document.querySelector('.login-form');
+  submitButtonElem.addEventListener('submit', submittingFormData); 
 
-const createUserForm = user => {
-  return fetch(baseUrl, {
+const createUserForm = user =>
+  fetch(baseUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
     },
     body: JSON.stringify(user),
   });
+
+const getUserForm = () => 
+  fetch(baseUrl)
+    .then(response => response.json())
+    .then(value => alert(JSON.stringify(value[value.length - 1])));
+
+const changeForm = () => {
+  emailElem.value = '';
+  textElem.value = '';
+  passwordElem.value = '';
 }
