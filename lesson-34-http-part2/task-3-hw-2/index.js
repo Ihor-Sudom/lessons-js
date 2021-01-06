baaa47cc7386d58366376b4bfd930bd64cf0ad1b
Ihor <ihor.sudom@gmail.com>
2021-01-06 09:44:40 +0200

    option

--- lesson-34-http-part2/task-3-hw-2/index.js
+++ lesson-34-http-part2/task-3-hw-2/index.js
@@ -4,7 +4,8 @@ const emailElem = document.querySelector('#email');
 const textElem = document.querySelector('#name');
 const passwordElem = document.querySelector('#password');
 
-battonElem = document.querySelector('.submit-button');
+const battonElem = document.querySelector('.submit-button');
+const errorElem = document.querySelector('.error-text');
 
 
 const onInputValid = () => {
@@ -27,28 +28,46 @@ const submittingFormData = (event) => {
     userName: textElem.value,
     password: passwordElem.value,
   };
- 
+
   createUserForm(user)
     .then(() => changeForm())
-    .then(() => getUserForm());
+    .catch((err) => errorElem.textContent = err)
+    .then(() => getUserForm())
+
 }
 
-  const submitButtonElem = document.querySelector('.login-form');
-  submitButtonElem.addEventListener('submit', submittingFormData); 
+const submitButtonElem = document.querySelector('.login-form');
+submitButtonElem.addEventListener('submit', submittingFormData);
 
-const createUserForm = user =>
+const createUserForm = user => {
+  return new Promise((resolve, reject) => {
+    if (baseUrl != 'https://5ff2e7d128c3980017b18ca3.mockapi.io/api/v1/form') {
+      reject('Failed to create user');
+    }
+    resolve(fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(user),
+    }));
+  })
+}
+
+
+/* const createUserForm = user =>
   fetch(baseUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
     },
     body: JSON.stringify(user),
-  });
+  }); */
 
-const getUserForm = () => 
+const getUserForm = () =>
   fetch(baseUrl)
-    .then(response => response.json())
-    .then(value => alert(JSON.stringify(value[value.length - 1])));
+  .then(response => response.json())
+  .then(value => alert(JSON.stringify(value[value.length - 1])));
 
 const changeForm = () => {
   emailElem.value = '';
