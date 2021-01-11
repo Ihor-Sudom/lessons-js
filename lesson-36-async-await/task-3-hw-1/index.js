4da8d1106c0da654fbbad856cbb65f4b0c433a73
Ihor <ihor.sudom@gmail.com>
2021-01-11 16:40:11 +0200

    option

--- lesson-36-async-await/task-3-hw-1/index.js
+++ lesson-36-async-await/task-3-hw-1/index.js
@@ -1,28 +1,9 @@
 
 
-
 export const getUsersBlogs = (...userId) => {
-  return Promise.all(userId.map(el => fetchUserData(el).then(userData => userData.blog)));
-};
-
- const fetchUserData = async userId => {
-  try {
-    const response = await fetch(`https://api.github.com/users/${userId}`);
-    if (response.ok) {
-      return response.json();
-    }
-  } catch(err) {
-    throw new Error(err.message);
-  };
-};
-
-
-
-
-/* const getUsersBlogs = (...userId) => {
   try {
     return Promise.all(userId.map(el => fetchUserData(el).then(userData => userData.blog)));
-  } catch (err) 
+  } catch (err) {
     throw new Error(err.message);
   }
 };
@@ -32,4 +13,4 @@ const fetchUserData = async userId => {
     if (response.ok) {
       return response.json();
     }
-}; */
+};
